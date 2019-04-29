86dcfc3471da7b0e395981050c24b500cf33a994
yingshan6839 <yingshan.guan83112@gmail.com>
2019-04-29 14:31:48 -0700

    first commit from post, try FlatList for the post list

--- components/Post.js
+++ components/Post.js
@@ -11,15 +11,9 @@ export default class Post extends Component<Props> {
       {key: 'A'},  {key: 'B'},  {key: 'c'}, {key: 'D'}
     ];
     return (
-      <View style={styles.container}>
-
-        <View style={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          padding: 40,
-          }}>
-            <Button 
+      <View >
+          <View style={styles.container}>
+            <Button
               onPress={this.onPress}
               title="Water"
               color="#841584"
@@ -39,57 +33,29 @@ export default class Post extends Component<Props> {
               title="Medicine"
               color="green"
             />
-        </View>
-          {/* <FlatList
+          </View>
+
+        
+       
+            <FlatList
             data={[{key: 'a'}, {key: 'b'}]}
             renderItem={({item}) => <Text>{item.key}</Text>}
-          /> */}
+          /> 
+     
 
-        
-
-        
-
-  </View>
-    );
+        </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'column',
-        backgroundColor: '#F5FCFF',
-        flex: 1,
-        justifyContent: 'space-between',
-      },
-
-      button:{
-        alignItems: 'center',
-        flex:1,
-        backgroundColor: 'blue',
-        padding: 25
-      },
-      btn_text:{
-        fontSize: 20,
-        color: 'black'
-      },
-      b2:{
-        alignItems: 'center',
-        flex:1,
-        backgroundColor: 'black',
-        padding: 25
-      },
-      b3:{
-        alignItems: 'center',
-        flex:1,
-        backgroundColor: 'green',
-        padding: 25
-      },
-      b4:{
-        alignItems: 'center',
-        flex:1,
-        backgroundColor: 'red',
-        padding: 25
-      }
+      flexDirection: 'row', 
+      justifyContent: 'center', 
+      justifyContent: 'space-between',
+      padding: 40
+    }
+      
 
 
 });
