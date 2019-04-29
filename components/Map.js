aa516fce654d8c054ed2effa936f0da9a7322bed
Amanda Bullington <albullington@gmail.com>
2019-04-28 23:35:49 -0700

    React native maps working on iOS

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,63 @@
 // @flow
 
-import React, { Component } from "react";
+import React from "react";
 import {
-  Image,
-  ScrollView,
-  Text,
   View,
-  SafeAreaView,
-  Platform
+  StyleSheet
 } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
+const region = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+};
 
 const MapScreen = () => {
-  <View>
-    <Text>this is the map</Text>
-  </View>
-}
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        provider={PROVIDER_DEFAULT}
+        style={styles.map}
+        region={region}
+        onRegionChangeComplete={region => console.log( region )}
+      />
+    </View>
+  );
+};
 
 export default MapScreen;
+
+const styles = StyleSheet.create( {
+  container: {
+    flex: 1
+  },
+  backButton: {
+    top: 28,
+    left: 23
+  },
+  image: {
+    padding: 5
+  },
+  mapContainer: {
+    flexGrow: 1
+  },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  markerFixed: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  markerPin: {
+    width: 23,
+    height: 33,
+    position: "absolute"
+  }
+} );
