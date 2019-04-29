91d216b3ef6383b9b9c95127c3ea630c4de3ef11
Amanda Bullington <albullington@gmail.com>
2019-04-28 20:44:12 -0700

    Updated app

---
+++ components/Map.js
@@ -0,0 +1,19 @@
+// @flow
+
+import React, { Component } from "react";
+import {
+  Image,
+  ScrollView,
+  Text,
+  View,
+  SafeAreaView,
+  Platform
+} from "react-native";
+
+const MapScreen = () => {
+  <View>
+    <Text>this is the map</Text>
+  </View>
+}
+
+export default MapScreen;
