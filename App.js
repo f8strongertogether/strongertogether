5629f7d0fe5b0c7094f45bd268f0c9b5884919b2
Wilder Pereira <wilder_roberto@hotmail.com>
2019-04-28 23:38:13 -0700

    Add simple sms listener

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import SmsRetriever from "react-native-sms-retriever";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,6 +19,31 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+    async componentDidMount() {
+        try {
+            const registered = await SmsRetriever.startSmsRetriever();
+
+            if (registered) {
+                SmsRetriever.addSmsListener(this._onReceiveSms);
+            }
+
+            alert(`SMS Listener Registered: ${registered}`);
+        } catch (error) {
+            alert(`SMS Listener Error: ${JSON.stringify(error)}`);
+        }
+    }
+
+
+    // Handlers
+    _onReceiveSms = (event) => {
+        alert(event.message
+            .replace('<#>', '')
+            .replace('Dt6yj3YDe6i', '')
+        );
+    };
+
+
   render() {
     return (
       <View style={styles.container}>
