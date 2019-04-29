5670bc184d01f7775eaea847f12acc95a63326e4
Amanda Bullington <albullington@gmail.com>
2019-04-28 19:57:26 -0700

    Updated readme

--- App.js
+++ App.js
@@ -21,9 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Stronger Together</Text>
+        <Text style={styles.instructions}>For disaster response</Text>
       </View>
     );
   }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# strongertogether
+## Getting started
+
+1. Run `npm start`
+2. Run `react-native run-ios` or `react-native run-android`
