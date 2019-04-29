9b4d62d709a461fdc68fd066c5f5fa58e38b229c
yingshan6839 <yingshan.guan83112@gmail.com>
2019-04-29 15:25:39 -0700

    latest version post

--- components/Post.js
+++ components/Post.js
@@ -11,24 +11,24 @@ export default class Post extends Component<Props> {
       {key: 'A'},  {key: 'B'},  {key: 'c'}, {key: 'D'}
     ];
     return (
-      <View >
+      <View style={{flex:1}}>
           <View style={styles.container}>
-            <Button
+            <Button style={flex=1}
               onPress={this.onPress}
               title="Water"
               color="#841584"
             />
-            <Button
+            <Button style={flex=1}
               onPress={this.onPress}
               title="Clothings"
               color="#DDDDDD"
             />
-            <Button
+            <Button style={flex=1}
               onPress={this.onPress}
               title="Evacuation"
               color="blue"
             />
-            <Button
+            <Button style={flex=1}
               onPress={this.onPress}
               title="Medicine"
               color="green"
@@ -50,10 +50,13 @@ export default class Post extends Component<Props> {
 
 const styles = StyleSheet.create({
     container: {
+      display: 'flex',
+      alignItems: 'flex-start',
+      flexWrap: 'wrap',
       flexDirection: 'row', 
-      justifyContent: 'center', 
-      justifyContent: 'space-between',
-      padding: 40
+      justifyContent: 'center',
+      paddingRight: 40,
+      paddingLeft: 40
     }
       
 
